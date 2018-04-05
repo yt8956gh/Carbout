9edfc71533c1d32ed2363a5b2b5a057ab99adee5
毛胤年 <yt8956789@gmail.com>
2018-04-05 23:19:10 +0800

    4/5 finish transport calculate

--- app/src/main/java/com/nian/carbout/Transport_Activity.java
+++ app/src/main/java/com/nian/carbout/Transport_Activity.java
@@ -1,13 +1,15 @@
 package com.nian.carbout;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.RadioGroup;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,7 +17,7 @@ public class Transport_Activity extends AppCompatActivity {
 
     private int  transport_answer=0;
     private int  TRA_answer=0;
-    private int  payWay_answer=0;
+    private int  payWay_answer=R.id.cash_pay;//注意初始值
 
 
     @Override
@@ -71,7 +73,6 @@ public class Transport_Activity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 TRA_answer=position;
-                //Toast.makeText(view.getContext(), item_name, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -84,17 +85,36 @@ public class Transport_Activity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String note = transport_answer+":"+TRA_answer+":"+
-                        ((payWay_answer==R.id.cash_pay)?"Cash":"Elec");
-                Toast.makeText(v.getContext(), note, Toast.LENGTH_SHORT).show();
 
-                if(transport_answer==0)//台鐵
+                EditText et = (EditText)findViewById(R.id.ticket_price);
+                String input = et.getText().toString();
+
+                //trim()回傳去除首尾空白符號的子字串
+                if("".equals(et.getText().toString().trim()))//如果輸入為空
                 {
+                    new AlertDialog.Builder(v.getContext())
+                            .setTitle("提醒")
+                            .setMessage("請輸入票價")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
 
+                                }
+                            }).show();
                 }
-                else //捷運
+                else
                 {
+                    int price = Integer.parseInt(input),co2 = 0;
+                    co2 = calculate_co2(price);
+                    new AlertDialog.Builder(v.getContext())
+                            .setTitle("計算完成")
+                            .setMessage("本次搭乘共消耗 "+co2+" Kg")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
 
+                                }
+                            }).show();
                 }
             }
         });
@@ -108,5 +128,52 @@ public class Transport_Activity extends AppCompatActivity {
         }
 
 
+        public int calculate_co2(int price)
+        {
+            int co2=0;
+
+            if(transport_answer==0)//台鐵
+            {
+                if(payWay_answer==R.id.cash_pay)//現金支付
+                {
+                    switch(TRA_answer)
+                    {
+                        case 0:
+                            co2 = (int)(price/1.46F*54);
+                            break;
+                        case 1:
+                            co2 = (int)(price/1.06F*54);
+                            break;
+                        case 2:
+                            co2 = (int)(price/1.46F*54);
+                            break;
+                        case 3:
+                            co2 = (int)(price/1.75F*54);
+                            break;
+                        case 4:
+                            co2 = (int)(price/2.27F*54);
+                            break;
+                    }
+                }
+                else if(payWay_answer==R.id.electronic_pay)//電子票證
+                {
+                    if(TRA_answer==4)//自強
+                    {
+                        if(price<=92) co2 = (int)(price/0.9F/1.46F*54);
+                        else co2 = 70*54 + (int)((price-92)/0.9F/1.46F*54);
+                    }
+                    else//其他
+                    {
+                        co2 = (int)(price/0.9F/1.46F*54);//電子票證九折優惠
+                    }
+                }
+            }
+            else //捷運
+            {
+                co2 = (int)((price/5-4)*3 + 3.5F);
+            }
+
+            return co2;
+        }
 
 }
