cd7b1640a3b018a64b60445aa625de73250504ed
毛胤年 <yt8956789@gmail.com>
2018-04-07 00:04:56 +0800

    SQLite some problems(ID Auto Increment)

---
+++ app/src/main/java/com/nian/carbout/DBhelper.java
@@ -0,0 +1,50 @@
+package com.nian.carbout;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DBhelper extends SQLiteOpenHelper {
+    public DBhelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE  TABLE main " +
+                "(_id INTEGER PRIMARY KEY AUTOINCREMENT , " +
+                "date VARCHAR NOT NULL , " +
+                "info VARCHAR NOT NULL, " +
+                "co2 INTEGER)");
+    }
+
+    // 當資料庫是較舊版時，呼叫execSQL刪除資料表，並呼叫onCreate重新建立資料庫
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS main");
+        onCreate(db);
+    }
+
+
+    public long append(SQLiteDatabase db, int co2, String info)
+    {
+        Long id;
+        Date dNow = new Date();//得到Date實例
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        ContentValues cv = new ContentValues();
+        cv.put("date", dateFormatter.format(dNow));
+        cv.put("info", info);
+        cv.put("co2", String.valueOf(co2));
+        id = db.insert("main",null,cv);
+
+        return id;
+    }
+}
--- app/src/main/java/com/nian/carbout/Transport_Activity.java
+++ app/src/main/java/com/nian/carbout/Transport_Activity.java
@@ -1,7 +1,10 @@
 package com.nian.carbout;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,19 +13,34 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.SimpleTimeZone;
 
 public class Transport_Activity extends AppCompatActivity {
 
     private int  transport_answer=0;
     private int  TRA_answer=0;
     private int  payWay_answer=R.id.cash_pay;//注意初始值
+    private int again=0;
+    private DBhelper dataHelper;
+    private SQLiteDatabase db;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transport);
 
-        final Spinner spinner_transport = (Spinner) findViewById(R.id.spinner_choose_transport);
+        // The database is not actually created or opened
+        // until one of getWritableDatabase() or getReadableDatabase() is called.
+        dataHelper = new DBhelper(this, "co2.sqlite",null, 1);
+
+        db = dataHelper.getWritableDatabase();
+
+        final Spinner spinner_transport = findViewById(R.id.spinner_choose_transport);
 
         //設定SpinnerAdapter(context, 數據來源, 清單樣式)
         ArrayAdapter<CharSequence> tAdapter = ArrayAdapter.createFromResource(
@@ -51,7 +69,7 @@ public class Transport_Activity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {}
         });
 
-        final Spinner spinner_TRA = (Spinner) findViewById(R.id.spinner_choose_TRA);
+        final Spinner spinner_TRA = findViewById(R.id.spinner_choose_TRA);
 
         ArrayAdapter<CharSequence> TRA_Adapter = ArrayAdapter.createFromResource(
                 this, R.array.TRA_item, android.R.layout.simple_spinner_item);
@@ -74,12 +92,12 @@ public class Transport_Activity extends AppCompatActivity {
         });
 
         //確認鍵
-        Button button = (Button)findViewById(R.id.button_in_transport);
+        Button button = findViewById(R.id.button_in_transport);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                EditText et = (EditText)findViewById(R.id.ticket_price);
+                EditText et = findViewById(R.id.ticket_price);
                 String input = et.getText().toString();
                 String notify,title;
 
@@ -88,14 +106,17 @@ public class Transport_Activity extends AppCompatActivity {
                 {
                     notify="請輸入票價";
                     title="提醒";
+                    again=1;
                     dialogShow(v, title, notify);
                 }
                 else
                 {
                     int price = Integer.parseInt(input);
                     float co2 = calculate_co2(price);
+                    long id;
 
                     title="計算結果";
+                    again=0;
 
                     if(co2>1)
                     {
@@ -110,6 +131,7 @@ public class Transport_Activity extends AppCompatActivity {
                         notify = "由於碳足跡過低，本次計算結果不列入";
                     }
 
+                    saveData(co2);
                     dialogShow(v, title, notify);
                 }
 
@@ -117,43 +139,46 @@ public class Transport_Activity extends AppCompatActivity {
         });
     }
 
-        //RadioGroup的執行方法
-        public void onSelect(View view) {
-            payWay_answer=view.getId();
+    public void saveData(float co2)
+    {
+
+        Long id = dataHelper.append(db, (int)co2, (transport_answer==0)?"台鐵":"捷運");
+        Toast.makeText(Transport_Activity.this, "ID: "+ id, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void onSelect(View view)
+    {
+        payWay_answer=view.getId();
+    }
+
+    public void dialogShow(View v, String title, String notify)
+    {
+        new AlertDialog.Builder(v.getContext())
+                .setTitle(title)
+                .setMessage(notify)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(again==0)
+                        {
+                            Transport_Activity.this.finish();//計算完成後結束現在的activity
+                        }
+                    }
+                })
+                .show();
         }
 
+    public float calculate_co2(int price)
+    {
+        float co2=0;
 
-        public void dialogShow(View v, String title, String notify)
+        if(transport_answer==0)//台鐵
         {
-            new AlertDialog.Builder(v.getContext())
-                    .setTitle(title)
-                    .setMessage(notify)
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Transport_Activity.this.finish();
-                        }
-                    })
-                    .setNeutralButton("再次輸入",new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            EditText et = (EditText)findViewById(R.id.ticket_price);
-                            et.setText("");
-                        }
-                    })
-                    .show();
-        }
-
-        public float calculate_co2(int price)
-        {
-            float co2=0;
-
-            if(transport_answer==0)//台鐵
+            if(payWay_answer==R.id.cash_pay)//現金支付
             {
-                if(payWay_answer==R.id.cash_pay)//現金支付
+                switch(TRA_answer)
                 {
-                    switch(TRA_answer)
-                    {
                         case 0://站票
                             co2 = price/1.46F*54;
                             break;
@@ -169,27 +194,27 @@ public class Transport_Activity extends AppCompatActivity {
                         case 4://自強
                             co2 = price/2.27F*54;
                             break;
-                    }
-                }
-                else if(payWay_answer==R.id.electronic_pay)//電子票證
-                {
-                    if(TRA_answer==4)//自強
-                    {
-                        if(price<=92) co2 = price/0.9F/1.46F*54;
-                        else co2 = 70*54 + (price-92)/0.9F/1.46F*54;
-                    }
-                    else//其他
-                    {
-                        co2 = price/0.9F/1.46F*54;//電子票證九折優惠
-                    }
                 }
             }
-            else //捷運
+            else if(payWay_answer==R.id.electronic_pay)//電子票證
             {
-                co2 = ((price/5-4)*3 + 3.5F)*0.08F;
+                if(TRA_answer==4)//自強
+                {
+                    if(price<=92) co2 = price/0.9F/1.46F*54;
+                    else co2 = 70*54 + (price-92)/0.9F/1.46F*54;
+                }
+                else//其他
+                {
+                    co2 = price/0.9F/1.46F*54;//電子票證九折優惠
+                }
             }
-
-            return co2;
         }
+        else //捷運
+        {
+            co2 = ((price/5-4)*3 + 3.5F)*0.08F;
+        }
+
+        return co2;
+    }
 
 }
